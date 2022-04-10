15a61274c64ff9470672c9263562e5083dbb516e
davidshushan <93195038+davidshushan@users.noreply.github.com>
2022-04-10 20:51:19 +0300

    Delete video-games directory

--- video-games/videoGames/models/Coupon.js
+++
@@ -1,8 +0,0 @@
-export default class Coupon {
-    constructor(id, couponName, Discount) {
-        this.id = id;
-        this.couponName = couponName;
-        this.Discount = Discount;
-    }
-
-}
